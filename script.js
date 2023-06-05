ab54fa46be82f272a34ccd91f3d19a23bb6b0963
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-06-05 17:29:29 -0400

    Update script.js
    getting match data back!!! we're getting closer to the fun part

--- script.js
+++ script.js
@@ -4,51 +4,60 @@ let userName = "SaveAsUntitled"
 
 //putting the URLs needed for each request together
 let accountRequest = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + userName + "?api_key=" + apiKey;
+var matchList = []
 
 async function fetchData(requestURL, value = false) {
     const response = fetch(requestURL)
-        .then(response => response.json())
+      .then(response => response.json())
       .then(jsonifiedResponse => {
 
       //extracting puuid from JSON file
         if (value === "puuid") {
-              const soloValue = jsonifiedResponse[value]
-              return soloValue;
+            const soloValue = jsonifiedResponse[value]
+            return soloValue;
         } 
       //extracting matches from JSON file
         else if (value === "match")
         {
-          return JSON.stringify(jsonifiedResponse, null, 4)
+          const fetchedMatches = JSON.stringify(jsonifiedResponse, null, 4)
+          return fetchedMatches
         }
-
+      
       });
-      return response;
+      return response
   }
 
 let promiseOutput = "";
-let puuid = fetchData(accountRequest, "puuid").then(puuid => secondRequest(puuid).then(matches => thirdRequest(matches)));
+let puuid = fetchData(accountRequest, "puuid").then(puuid => secondRequest(puuid)).then(matches => thirdRequest(matches));
 
 async function secondRequest(puuid){
   //set variables so we can grab 1000 games
-  let matchNumber = 0
-  var matchList = []
+  let matchNumber = 0;
+  let x = 0;
   
   while (matchNumber < 1000) {
     // 450 is the queue value for ARAMs
     let matchRequest = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?queue=450&start=" + matchNumber + "&count=100&api_key=" + apiKey;
-    
-    //request 100 matches from API and add to match list array
-    fetchData(matchRequest, "match").then(batchMatches => console.log(batchMatches));
-    //fetchData(matchRequest, "match").then(batchMatches => matchList.push(batchMatches));
-    
+    //request 100 matches from API
+    fetchData(matchRequest, "match")
+
+      //clean up array to just have matches
+      .then(batchMatches => batchMatches.split("\""))
+      .then(removeSpace => removeSpace.filter(blanksBegone => blanksBegone.length == 14))
+      .then(cleanArray => thirdRequest(cleanArray))
+
     //adjust matches being requested to the next batch of 100
     matchNumber = matchNumber + 100;
   }
-
   //send list of matches out of secondRequest function to be used to request match info
   return matchList  
 }
 
 async function thirdRequest(matches){
-  console.log(matches)
+  for (let x = 0; x < matches.length; x++) {
+    let matchInfoRequest = "https://americas.api.riotgames.com/lol/match/v5/matches/" + matches[x] + "?api_key=" + apiKey
+    console.log(matchInfoRequest)
+    fetchData(matchInfoRequest, "match")
+      .then(info => console.log(info))
+  }
 }
