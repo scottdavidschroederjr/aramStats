e5f1991e16c5c0fd915b06338db9e3afd7f78efa
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-06-04 23:20:40 -0400

    got to the point where I can request matches but not sure if they're properly being added to an array

--- script.js
+++ script.js
@@ -6,26 +6,49 @@ let userName = "SaveAsUntitled"
 let accountRequest = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + userName + "?api_key=" + apiKey;
 
 async function fetchData(requestURL, value = false) {
-      const response = await fetch(requestURL);
-      const data = await response.json();
-      
-       // if only one piece is needed, returning just that
-    if (value != false) {
-      const soloValue = data[value]
-      return soloValue
-    }
-    else {
-      //future area for future pulls
-    }
+    const response = fetch(requestURL)
+        .then(response => response.json())
+      .then(jsonifiedResponse => {
+
+      //extracting puuid from JSON file
+        if (value === "puuid") {
+              const soloValue = jsonifiedResponse[value]
+              return soloValue;
+        } 
+      //extracting matches from JSON file
+        else if (value === "match")
+        {
+          return JSON.stringify(jsonifiedResponse, null, 4)
+        }
+
+      });
+      return response;
   }
 
-async function getData(requestURL, value = false) {
-    let result = await fetchData(requestURL, value);
-    return result
+let promiseOutput = "";
+let puuid = fetchData(accountRequest, "puuid").then(puuid => secondRequest(puuid).then(matches => thirdRequest(matches)));
+
+async function secondRequest(puuid){
+  //set variables so we can grab 1000 games
+  let matchNumber = 0
+  var matchList = []
+  
+  while (matchNumber < 1000) {
+    // 450 is the queue value for ARAMs
+    let matchRequest = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?queue=450&start=" + matchNumber + "&count=100&api_key=" + apiKey;
+    
+    //request 100 matches from API and add to match list array
+    fetchData(matchRequest, "match").then(batchMatches => console.log(batchMatches));
+    //fetchData(matchRequest, "match").then(batchMatches => matchList.push(batchMatches));
+    
+    //adjust matches being requested to the next batch of 100
+    matchNumber = matchNumber + 100;
   }
 
-puuid = getData(accountRequest, "puuid")
+  //send list of matches out of secondRequest function to be used to request match info
+  return matchList  
+}
 
-console.log(puuid)
-
-
+async function thirdRequest(matches){
+  console.log(matches)
+}
